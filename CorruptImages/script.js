6637956fff7698a38fc6cac1c58e4ad731f2dccd
Ziegenhainy <hawipru@gmail.com>
2026-01-24 21:39:14 +0100

    same jit

--- CorruptImages/script.js
+++ CorruptImages/script.js
@@ -1,10 +1,20 @@
 var allImages = document.getElementsByClassName("Image")
 
+imagePreviewContainer.onclick = ()=>{
+    imagePreviewContainer.style.opacity = 0;
+    imagePreviewContainer.style.pointerEvents = "none";
+}
+
 Array.from(allImages).forEach(div => {
-    image = div.getElementsByTagName("img").item(0)
+    let image = div.getElementsByTagName("img").item(0)
     if (image.complete) {
         div.classList.add("fadein");
     } else image.onload = () => {
         div.classList.add("fadein");
+    };
+    image.onclick = ()=>{
+        imagePreviewContainer.style.opacity = 1;
+        imagePreviewContainer.style.pointerEvents = "all";
+        imagePreview.src = image.src;
     };
 });
